9320e8fced66e53be92ddf83be292d01ef1f2f39
Fabien Culot <fab.culot@gmail.com>
2019-07-03 19:46:36 +0200

    fix captcha

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {!! NoCaptcha::renderJs() !!}
 </head>
 <body>
     <div id="app">
--- resources/views/layouts/portefeuille.blade.php
+++ resources/views/layouts/portefeuille.blade.php
@@ -247,8 +247,9 @@
         <div id="sidebar" class="nav-collapse ">
             <!-- sidebar menu start-->
             <ul class="sidebar-menu" id="nav-accordion">
-                <p class="centered"><a href="profile.html"><img src="img/ui-sam.jpg" class="img-circle" width="80"></a></p>
-                <h5 class="centered">Sam Soffes</h5>
+                <h4 class="centered">Bonjour !</h4>
+                <?php $user = Auth::user(); ?>
+                <h5 class="centered"><?= $user->name;  ?></h5>
                 <li class="mt">
                     <a class="active" href="/dashboard">
                         <i class="fa fa-dashboard"></i>
